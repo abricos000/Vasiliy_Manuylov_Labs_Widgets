6e0c5793b24b1972da7644b8b9935fee0cd00dc7
Vasiliy <manv_v@mail.ru>
2022-05-06 16:05:45 +0700

    Инициализация методов класса Win.cpp согласно заданию 2.8

--- win.cpp
+++ win.cpp
@@ -3,82 +3,34 @@
 Win::Win(QWidget *parent)
     : QWidget(parent)
 {
+    setWindowTitle("Счетчик");
 
-    setWindowTitle("Возведение в квадрат");
+    label1 = new QLabel("Cчет по 1", this);
+    label2 = new QLabel("Cчет по 5", this);
+    edit1 = new Counter("0", this);
+    edit2 = new Counter("0", this);
+    calcbutton = new QPushButton("+1", this);
+    exitbutton = new QPushButton("Выход", this);
 
-    frame = new QFrame(this);//Делаем выделение памяти для рамки
-    frame->setFrameShadow(QFrame::Raised);
-    frame->setFrameShape(QFrame::Panel);
+    QHBoxLayout *layout1 = new QHBoxLayout();
+    layout1->addWidget(label1);
+    layout1->addWidget(label2);
 
-    inputLabel = new QLabel("Введите число:", this);//Текст к полю ввода числа
-    inputEdit = new QLineEdit("", this);// Поле ввода числа
-    outputLabel = new QLabel("Результат:", this);//Текст к полю выводв результата
-    outputEdit = new QLineEdit("", this);//Поле вывода результата
-    nextButton = new QPushButton("Следующее", this);//кнопка для следующего вычесления
-    exitButton = new QPushButton("Выход", this);//Кнопка выхода
+    QHBoxLayout *layout2 = new QHBoxLayout();
+    layout2->addWidget(edit1);
+    layout2->addWidget(edit2);
 
-    QVBoxLayout *vLayout1 = new QVBoxLayout(frame);//создание объекта
-    //добавление полей в BoxLayout
-    vLayout1->addWidget(inputLabel);
-    vLayout1->addWidget(inputEdit);
-    vLayout1->addWidget(outputLabel);
-    vLayout1->addWidget(outputEdit);
-    vLayout1->addStretch();
+    QHBoxLayout *layout3 = new QHBoxLayout();
+    layout3->addWidget(calcbutton);
+    layout3->addWidget(exitbutton);
 
-    QVBoxLayout *vLayout2 = new QVBoxLayout();//создание объекта
-    //добавление полей в BoxLayout
-    vLayout2->addWidget(nextButton);
-    vLayout2->addWidget(exitButton);
-    vLayout2->addStretch();
+    QVBoxLayout *layout4 = new QVBoxLayout(this);
+    layout4->addLayout(layout1);
+    layout4->addLayout(layout2);
+    layout4->addLayout(layout3);
 
-    QHBoxLayout *hLayout = new QHBoxLayout(this);//создание объекта
-    //добавление полей в BoxLayout
-    hLayout->addWidget(frame);
-    hLayout->addLayout(vLayout2);
-
-    begin();
-   //Сигналы
-    connect(exitButton, &QPushButton::clicked, this, &Win::close);//закрытие окна
-    connect(nextButton, &QPushButton::clicked, this, &Win::begin);//переход к следующему вычислению
-    connect(inputEdit, &QLineEdit::returnPressed, this, &Win::calc);//вывод результата при нажатии клавиши Enter
-}
-void Win::begin()
-{
-    inputEdit->clear();//удаление предыдущего результата
-    nextButton->setEnabled(false);//отлючаем кнопку
-    inputEdit->setEnabled(true);//активируем поле
-    outputLabel->setVisible(false);//виджет невидим
-    outputEdit->setVisible(false);//виджет невидим
-    outputEdit->setEnabled(false);//отлючаем ввод в данное поле
-    inputEdit->setFocus();//ставим фокус
-}
-
-void Win::calc()
-{
-    bool Ok = true;
-    float r, a;
-
-    QString str = inputEdit->text();
-    a = str.toDouble(&Ok);
-
-    if (Ok)
-    {
-        r = a * a;
-
-        str.setNum(r);
-
-        outputEdit->setText(str);//вывод результата
-        inputEdit->setEnabled(false);//отлючаем ввод в данное поле
-        outputLabel->setVisible(true);//виджет видим
-        outputEdit->setVisible(true);//виджет видим
-        nextButton->setDefault(true);
-        nextButton->setEnabled(true);//активируем кнопку
-        nextButton->setFocus();//ставим фокус
-    }
-    else if (!str.isEmpty())
-    {
-        //вывод сообщения об ошибке
-        QMessageBox msgBox(QMessageBox::Information, "Возведение в квадрат.", "Введено неверное значение.", QMessageBox::Ok);
-        msgBox.exec();
-    }
+    // связь сигнала нажатия кнопки и слота закрытия окна
+    connect(calcbutton, &QPushButton::clicked, edit1, &Counter::add_one);
+    connect(edit1, &Counter::tick_signal, edit2, &Counter::add_one);
+    connect(exitbutton, &QPushButton::clicked, this, &Win::close);
 }
