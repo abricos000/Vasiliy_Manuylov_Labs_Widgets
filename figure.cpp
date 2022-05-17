a8a7d58961207f2f8ea3326ebbfddef14a6e1d60
Vasiliy <manv_v@mail.ru>
2022-05-17 17:56:26 +0700

    Инициализация методов в классе figure

--- figure.cpp
+++ figure.cpp
@@ -1,7 +1,20 @@
-#include "figure.h"
-
-Figure::Figure(QWidget *parent)
-    : QWidget{parent}
+#include <math.h>
+#include "figura.h"
+void Figura::move(float Alpha,QPainter *Painter)
 {
-
+    dx=halflen*cos(Alpha);//координата вращения по оси x
+    dy=halflen*sin(Alpha);//координата вращения по оси y
+    draw(Painter);
+}
+void MyLine::draw(QPainter *Painter)
+{
+    Painter->drawLine(x+dx,y+dy,x-dx,y-dy);//отрисовка линии после вращения
+}
+void MyRect::draw(QPainter *Painter)
+{
+    //Отрисовываем линии стенок квадрата после вращения
+    Painter->drawLine(x+dx,y+dy,x+dy,y-dx);
+    Painter->drawLine(x+dy,y-dx,x-dx,y-dy);
+    Painter->drawLine(x-dx,y-dy,x-dy,y+dx);
+    Painter->drawLine(x-dy,y+dx,x+dx,y+dy);
 }
--- figure.h
+++ figure.h
@@ -1,16 +1,31 @@
-#ifndef FIGURE_H
-#define FIGURE_H
+#ifndef FIGURA_H
+#define FIGURA_H
 
-#include <QWidget>
-
-class Figure : public QWidget
+#include <QtGui>
+class Figura
 {
-    Q_OBJECT
-public:
-    explicit Figure(QWidget *parent = nullptr);
-
-signals:
-
+    protected:
+        int x,y,halflen,dx,dy,r;//определение переменных координат и размера фигуры
+        virtual void draw(QPainter *Painter)=0;
+    public:
+        Figura(int X,int Y,int Halflen):
+        x(X),y(Y),halflen(Halflen){}//конструктор размера и положения объекта
+        void move(float Alpha,QPainter *Painter);
 };
-
-#endif // FIGURE_H
+//Объявление класса линии
+class MyLine:public Figura
+{
+    protected:
+        void draw(QPainter *Painter);//метод рисования линии
+    public:
+        MyLine(int x,int y,int halflen):Figura(x,y,halflen){}//конструктор размерова и позиции
+};
+//Объявление класса квадрата
+class MyRect:public Figura
+{
+    protected:
+        void draw(QPainter *Painter);//метод рисования квадрата
+    public:
+        MyRect(int x,int y,int halflen):Figura(x,y,halflen){}//конструктор размера и позиции
+};
+#endif // FIGURA_H
