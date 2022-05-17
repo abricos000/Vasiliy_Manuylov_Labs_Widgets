f2f2cf3f30ffb053491e91b93c20ea239c73924f
Vasiliy <manv_v@mail.ru>
2022-05-17 18:17:04 +0700

    Инициализация методов  в классе Area согласно методичке

--- area.cpp
+++ area.cpp
@@ -1,7 +1,45 @@
 #include "area.h"
-
-area::area(QWidget *parent)
-    : QWidget{parent}
+Area::Area(QWidget *parent):QWidget(parent)
 {
-
+    setFixedSize(QSize(300,200));//параметры интерфейса
+    myline=new MyLine(80,100,50);//размер и позиция линии
+    myrect=new MyRect(220,100,50);//размер и позиция квадрата
+    alpha=0;//угол разворота
 }
+//метод включения таймера
+void Area::showEvent(QShowEvent *)
+{
+    myTimer=startTimer(50); // создать таймер
+}
+//метод отрисовки перемещающихся фигур;
+void Area::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    painter.setPen(Qt::red);//цвет Pen
+    myline->move(alpha,&painter);//разворот линии
+    myrect->move(alpha*(-0.5),&painter);//разворот квадрата
+}
+//метод перерисовки Холста;
+void Area::timerEvent(QTimerEvent *event)
+{
+    if (event->timerId() == myTimer) // если наш таймер
+    {
+        alpha=alpha+0.2;//меняем угол
+        update(); // обновить внешний вид
+    }
+    else
+        QWidget::timerEvent(event); // иначе передать для стандартной
+// обработки
+}
+//метод выключения таймера
+void Area::hideEvent(QHideEvent *)
+{
+    killTimer(myTimer); // уничтожить таймер
+}
+Area::~Area()
+{
+    delete myline;
+    delete myrect;
+}
+© 2022 GitHub, Inc.
+Term
--- area.h
+++ area.h
@@ -1,16 +1,22 @@
 #ifndef AREA_H
 #define AREA_H
 
+#include "figura.h"
 #include <QWidget>
-
-class area : public QWidget
-{
-    Q_OBJECT
-public:
-    explicit area(QWidget *parent = nullptr);
-
-signals:
-
+class Area : public QWidget
+{//поля класса
+    int myTimer; // идентификатор таймера
+    float alpha; // угол поворота
+    public:
+    Area(QWidget *parent = 0);
+    ~Area();
+    MyLine *myline;//Создание экземпляра объекта линии
+    MyRect *myrect;//Создание экземпляра объект квадрата
+protected:
+//обработчики событий
+    void paintEvent(QPaintEvent *event);//рисование пошагово перемещающихся фигур;
+    void timerEvent(QTimerEvent *event);//инициация перерисовки Холста;
+    void showEvent(QShowEvent *event);//включение таймера;
+    void hideEvent(QHideEvent *event);//выключение таймера.
 };
-
 #endif // AREA_H
